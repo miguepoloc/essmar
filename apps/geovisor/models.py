929fc47642517ea9a014f9d2e53ddc541afa10de
miguepoloc <miguepoloc@gmail.com>
2020-02-07 01:13:17 -0500

    Creacion del modelo de luminarias

--- apps/geovisor/models.py
+++ apps/geovisor/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Luminarias(models.Model):
+    id = models.IntegerField(primary_key=True)
+    nombre_ciudad = models.CharField(max_length=200)
+    nombre_barrio = models.CharField(max_length=200)
+    codigo_actual = models.CharField(max_length=200, blank=True, null=True)
+    nombre_estructura = models.CharField(max_length=200)
+    potencia = models.IntegerField()
+    nombre_uso_alumbrado = models.CharField(max_length=200)
+    x = models.CharField(max_length=200)
+    y = models.CharField(max_length=200)
+    latitud = models.CharField(max_length=200, blank=True, null=True)
+    longitud = models.CharField(max_length=200, blank=True, null=True)
+    nombre_tipo_fuente = models.CharField(max_length=200)
+    nombre_tipo_luminaria = models.CharField(max_length=200)
+    fecha_inventario = models.DateTimeField()
+
+    def __str__(self):
+        return self.nombre_ciudad
